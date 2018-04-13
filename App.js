500ac5c1c48d186f1141af7ed90b2937a57862c0
Hunter Rafuse <31886722+HunterRafuse@users.noreply.github.com>
2018-04-13 12:17:32 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import Home from './src/components/Home';
+import Chat from './src/components/Chat';
+import {
+  Router,
+  Scene,
+} from 'react-native-router-flux';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Scene key='root' style={{paddingTop: Platform.OS === 'android' ? 64 : 54}}>
+          <Scene key='home' component={Home} title='Home' />
+          <Scene key='chat' component={Chat} title='Chat' />
+        </Scene>
+      </Router>
+    );
+  }
+}
+
+export default app;
